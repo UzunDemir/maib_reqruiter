f11f59f913b1f40189c4ae9ca8027a56d35ac634
Uzun Demir <94790150+UzunDemir@users.noreply.github.com>
2025-05-20 11:46:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,22 +20,22 @@ tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 st.set_page_config(layout="wide", initial_sidebar_state="auto")
 st.markdown("""
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, 
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, 
-    .viewerBadge_text__1JaDK, #MainMenu, footer, header { 
-        display: none !important; 
-    }
-    .center {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-        flex-direction: column;
-        margin-top: 0vh;
-    }
-</style>
-""", unsafe_allow_html=True)
+# <style>
+#     .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, 
+#     .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, 
+#     .viewerBadge_text__1JaDK, #MainMenu, footer, header { 
+#         display: none !important; 
+#     }
+#     .center {
+#         display: flex;
+#         justify-content: center;
+#         align-items: center;
+#         text-align: center;
+#         flex-direction: column;
+#         margin-top: 0vh;
+#     }
+# </style>
+# """, unsafe_allow_html=True)
 ################################
 
 import streamlit as st
